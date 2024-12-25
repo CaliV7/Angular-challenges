69bcfd53857c15052b5569917370b606894b6a89
CaliV7 <calistavolget@gmail.com>
2024-12-25 20:53:56 +0100

    create a new projet and create a component block which will be called 4 times on the page. Adding some css with tailwind

---
+++ challenge2/src/app/block/block.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-block',
+  imports: [],
+  templateUrl: './block.component.html',
+  styleUrl: './block.component.css'
+})
+export class BlockComponent {
+
+}
